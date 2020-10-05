ce5b2efd936e8af8894ee7f2188757315c48d033
Tao130 <momossy5@gmail.com>
2020-10-05 17:35:51 +0900

    SAMtoolsの実行スレッドを2に変更

--- work/scripts/samtobam.sh
+++ work/scripts/samtobam.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-samtools sort -@ 16 -O bam -o /work/results/bam_result/sort1.bam /work/results/mapping_result/result1.sam 
+# -@オプションでソート処理の並列化(複数スレッドでの実行)ができる。環境に応じて数値を変更
+samtools sort -@ 2 -O bam -o /work/results/bam_result/sort1.bam /work/results/mapping_result/result1.sam 
